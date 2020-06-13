cc0d954a996aedfb1efebdfd4f66832b597fdea2
HKD852 <gomyong@gmail.com>
2020-06-13 21:00:07 +0900

    22

---
+++ colors.js
@@ -0,0 +1,45 @@
+var Links =
+{
+  setColor:function(color)
+  {
+    var alist = document.querySelectorAll('a')
+    var i = 0;
+    while(i < alist.length)
+    {
+      alist[i].style.color = color;
+      i = i + 1;
+    }
+  }
+}
+var Body =
+{
+  setColor:function(color)
+  {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function(color)
+  {
+    document.querySelector('body').style.backgroundColor=color;
+  }
+}
+
+function nightDayHandler(self)
+  {
+    var target = document.querySelector('body');
+    if(self.value === '夜 NIGHT')
+    {
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      self.value = '日 DAY'
+
+      Links.setColor('powderblue');
+    }
+    else
+    {
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      self.value = '夜 NIGHT'
+
+      Links.setColor('blue');
+      }
+  }
